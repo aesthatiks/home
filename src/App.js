15b9115d4572b7eb595467b1808668be1b32a3a9
Amar <amar@Amars-MacBook-Pro-2.local>
2020-02-03 21:03:02 +0100

    About and My Work pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
+import About from './components/About';
+import MyWork from './components/MyWork';
 import Navbar from './components/Navbar';
 import './App.css';
 
@@ -11,6 +13,8 @@ function App() {
       <section className='container'>
         <Switch>
           <Route exact path='/' component={Landing} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/mywork' component={MyWork} />
         </Switch>
       </section>
     </Router>
