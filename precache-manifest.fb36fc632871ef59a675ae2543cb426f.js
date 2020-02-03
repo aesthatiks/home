5a2439e3acc4de482d1bc5aad00b571995fbe0c7
Amar <amar@Amars-MacBook-Pro-2.local>
2020-02-03 20:33:32 +0100

    Updates

---
+++ precache-manifest.fb36fc632871ef59a675ae2543cb426f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7efcea83f5accca9d302a3dd7622beb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "2241ce48273a7690be40",
+    "url": "/home/static/css/main.b4509f78.chunk.css"
+  },
+  {
+    "revision": "1cd256d7cb3ed1d43a7a",
+    "url": "/home/static/js/2.981469d2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/home/static/js/2.981469d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2241ce48273a7690be40",
+    "url": "/home/static/js/main.c93299de.chunk.js"
+  },
+  {
+    "revision": "e2ecd50f7898552ba4c4",
+    "url": "/home/static/js/runtime-main.cbe70bf9.js"
+  },
+  {
+    "revision": "dbd127b448013e846163125c48207855",
+    "url": "/home/static/media/amar.dbd127b4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b926a670b9ad4080c131827c505c396c.js"
+  "/home/precache-manifest.fb36fc632871ef59a675ae2543cb426f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
