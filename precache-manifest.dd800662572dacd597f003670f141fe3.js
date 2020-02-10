40d2e3423baa1d1758fd1f9449799fcfe520b544
Amar <amar@Amars-MacBook-Pro-2.local>
2020-02-10 23:55:27 +0100

    Updates

---
+++ precache-manifest.dd800662572dacd597f003670f141fe3.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb5add2ea964fd385b58b9810afcc50",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "379dcff4bcc67dab63d4",
+    "url": "/home/static/css/main.2bd4d3a5.chunk.css"
+  },
+  {
+    "revision": "952c8a7b69ad2032ef11",
+    "url": "/home/static/js/2.5188e64e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/home/static/js/2.5188e64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379dcff4bcc67dab63d4",
+    "url": "/home/static/js/main.c060360e.chunk.js"
+  },
+  {
+    "revision": "e2ecd50f7898552ba4c4",
+    "url": "/home/static/js/runtime-main.cbe70bf9.js"
+  },
+  {
+    "revision": "0784c30ec63c853808ddba44ee300522",
+    "url": "/home/static/media/Resume.0784c30e.pdf"
+  },
+  {
+    "revision": "b08279c4d00c1370f84dd1cdd305cb6d",
+    "url": "/home/static/media/allexis-edu.b08279c4.png"
+  },
+  {
+    "revision": "dbd127b448013e846163125c48207855",
+    "url": "/home/static/media/amar.dbd127b4.png"
+  },
+  {
+    "revision": "4fbbe969e93e021822b9beb967faac08",
+    "url": "/home/static/media/appletv-details.4fbbe969.jpg"
+  },
+  {
+    "revision": "954bc704af39d0054e384dd42c2dc0d4",
+    "url": "/home/static/media/appletv-epg.954bc704.jpg"
+  },
+  {
+    "revision": "70040a7a7880c2843dc39d95b79d5972",
+    "url": "/home/static/media/appletv-home.70040a7a.jpg"
+  },
+  {
+    "revision": "26ac708bd9a2c229e01fede7de100888",
+    "url": "/home/static/media/cover.26ac708b.jpg"
+  },
+  {
+    "revision": "32eaa603cfdc7bddf08401d73094ae86",
+    "url": "/home/static/media/ios-details-w.32eaa603.jpg"
+  },
+  {
+    "revision": "121175e4f6911b39877c881c41dd7a94",
+    "url": "/home/static/media/ios-details.121175e4.jpg"
+  },
+  {
+    "revision": "1325c646eb561ad3cb9f16d4a8b83a66",
+    "url": "/home/static/media/ios-epg-w.1325c646.jpg"
+  },
+  {
+    "revision": "2a9efebf34ec1197f182bd1ee1e25813",
+    "url": "/home/static/media/ios-epg.2a9efebf.jpg"
+  },
+  {
+    "revision": "813ae52c63b65c3de4dae3184a33a1c5",
+    "url": "/home/static/media/ios-home-w.813ae52c.jpg"
+  },
+  {
+    "revision": "d59bd2645a4cc6832eb37f8a846a0766",
+    "url": "/home/static/media/ios-home.d59bd264.jpg"
+  },
+  {
+    "revision": "ad549fb08deecd45381e34cddd9586a8",
+    "url": "/home/static/media/stb-details.ad549fb0.jpg"
+  },
+  {
+    "revision": "515c84437d0a48ffd865856776cb4377",
+    "url": "/home/static/media/stb-epg.515c8443.jpg"
+  },
+  {
+    "revision": "7e49caf5714dc362d80dda37da7bcb42",
+    "url": "/home/static/media/stb-home.7e49caf5.jpg"
+  },
+  {
+    "revision": "9825240840dfb770a1fc1f3e8891ce3c",
+    "url": "/home/static/media/web-details.98252408.jpg"
+  },
+  {
+    "revision": "07acb01c28a7ffd9f9784db7497e2f2f",
+    "url": "/home/static/media/web-epg.07acb01c.jpg"
+  },
+  {
+    "revision": "355df70f7bf47068f42dc0ed0ba33db7",
+    "url": "/home/static/media/web-home.355df70f.jpg"
+  }
+]);
